8fb09caa20242c87e96e0ab88ebcd4650683bc3f
hcqmaker@gmail.com <hcqmaker@gmail.com>
2013-12-30 16:55:26 +0800

    [motify] change path

---
+++ common/network.hpp
@@ -0,0 +1,21 @@
+#ifndef __NETWORK_HPP__
+#define __NETWORK_HPP__
+
+#include "base/ByteBuffer.hpp"
+#include "base/ByteConverter.hpp"
+#include "base/Config.hpp"
+#include "base/Crypto.hpp"
+#include "base/DbLib.hpp"
+#include "base/Define.hpp"
+#include "base/ICommon.hpp"
+#include "base/Log.hpp"
+#include "base/LogDefine.hpp"
+#include "base/NetClient.hpp"
+#include "base/NetLib.hpp"
+#include "base/NetQueue.hpp"
+#include "base/NetServer.hpp"
+#include "base/NetSession.hpp"
+#include "base/Packet.hpp"
+#include "base/Singleton.hpp"
+
+#endif // __NETWORK_HPP__
